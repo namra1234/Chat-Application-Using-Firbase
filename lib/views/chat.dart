f3d7dbd69f05bf4f07a6c6f401628bf4f4c5dc50
Namra <namra.patel@bosc.in>
2021-02-05 23:00:42 +0530

    Set Time with Message

--- lib/views/chat.dart
+++ lib/views/chat.dart
@@ -3,6 +3,7 @@ import 'package:chat_app_firebase/common/constants.dart';
 import 'package:chat_app_firebase/repository/chatChannelRep.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:intl/intl.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -334,43 +335,72 @@ class MessageTile extends StatelessWidget {
       onDoubleTap: () {
         ChatChannelRepository().deleteMessage(chatchannelId, messageId);
       },
-      child: Container(
-        padding: EdgeInsets.only(
-            top: 8,
-            bottom: 8,
-            left: sendByMe ? 0 : 24,
-            right: sendByMe ? 24 : 0),
-        alignment: sendByMe ? Alignment.centerRight : Alignment.centerLeft,
-        child: Container(
-          margin:
-              sendByMe ? EdgeInsets.only(left: 30) : EdgeInsets.only(right: 30),
-          padding: EdgeInsets.only(top: 17, bottom: 17, left: 20, right: 20),
-          decoration: BoxDecoration(
-              borderRadius: sendByMe
-                  ? BorderRadius.only(
-                      topLeft: Radius.circular(23),
-                      topRight: Radius.circular(23),
-                      bottomLeft: Radius.circular(23))
-                  : BorderRadius.only(
-                      topLeft: Radius.circular(23),
-                      topRight: Radius.circular(23),
-                      bottomRight: Radius.circular(23)),
-              gradient: LinearGradient(
-                colors: sendByMe
-                    ? [ColorConstants.kPrimaryColor, const Color(0xff2A75BC)]
-                    : [
-                        ColorConstants.kGreyColor,
-                        ColorConstants.kGreyColor,
-                      ],
-              )),
-          child: Text(message,
-              textAlign: TextAlign.start,
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 16,
-                  fontFamily: 'OverpassRegular',
-                  fontWeight: FontWeight.w300)),
-        ),
+      child: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.only(
+                top: 8,
+                bottom: 8,
+                left: sendByMe ? 0 : 24,
+                right: sendByMe ? 24 : 0),
+            alignment: sendByMe ? Alignment.centerRight : Alignment.centerLeft,
+            child: Column(
+              children: [
+                Container(
+                  margin:
+                      sendByMe ? EdgeInsets.only(left: 30) : EdgeInsets.only(right: 30),
+                  padding: EdgeInsets.only(top: 17, bottom: 17, left: 20, right: 20),
+                  decoration: BoxDecoration(
+                      borderRadius: sendByMe
+                          ? BorderRadius.only(
+                              topLeft: Radius.circular(23),
+                              topRight: Radius.circular(23),
+                              bottomLeft: Radius.circular(23))
+                          : BorderRadius.only(
+                              topLeft: Radius.circular(23),
+                              topRight: Radius.circular(23),
+                              bottomRight: Radius.circular(23)),
+                      gradient: LinearGradient(
+                        colors: sendByMe
+                            ? [ColorConstants.kPrimaryColor, const Color(0xff2A75BC)]
+                            : [
+                                ColorConstants.kGreyColor,
+                                ColorConstants.kGreyColor,
+                              ],
+                      )),
+                  child: Column(
+                    children: [
+                      Text(message,
+                          textAlign: TextAlign.start,
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                              fontFamily: 'OverpassRegular',
+                              fontWeight: FontWeight.w300),
+                              ),
+                              
+                    ],
+                  ),
+                ),
+                
+              ],
+            ),
+          ),
+          Container(
+                  alignment: sendByMe ? Alignment.centerRight : Alignment.centerLeft,
+                   margin:
+                      sendByMe ? EdgeInsets.only(left: 30) : EdgeInsets.only(right: 30),
+                  padding: EdgeInsets.only(top: 1, bottom: 1, left: 20, right: 20),
+                  child: Text(DateFormat('dd-MM-yy  HH:mm').format(date),
+                    textAlign: TextAlign.start,                    
+                    style: TextStyle(
+                        color: ColorConstants.kPrimaryColor,
+                        fontSize: 10,
+                        fontFamily: 'OverpassRegular',
+                        fontWeight: FontWeight.w500)),
+                )
+          
+        ],
       ),
     );
   }
